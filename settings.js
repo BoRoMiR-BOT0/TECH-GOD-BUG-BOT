62a98ce05696d933442ba3bcf6f56fb5bc0c8203
BoRoMiR-BOT0 <boromir.bot0@gmail.com>
2024-09-21 16:38:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "0757210624"
+global.ownername = "☣♱☠"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
@@ -40,7 +40,7 @@ global.prefa = ['','!','.','#','&']
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = true //auto typing + recording
-global.autoread = false //auto read messages
+global.autoread = true //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
